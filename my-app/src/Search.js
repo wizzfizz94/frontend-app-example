82280688d0579de35b152b1ad9b92f7515999972
wizzfizz94 <21149116@student.uwa.edu.au>
2022-05-04 11:39:20 +0800

    Basic image search built.

--- my-app/src/Search.js
+++ my-app/src/Search.js
@@ -2,19 +2,23 @@ import React, { useState } from 'react';
 
 function Search({ name }) {
   const [query, setQuery] = useState(null);
+  const [results, setResults] = useState([])
 
   return (
     <>
       <h1>Hello {name}.</h1>
       <form onSubmit={e => {
         fetch(`http://localhost:3001/search?query=${query}`, {mode: "cors"})
-          .then(res => res.text())
-          .then(text => console.log(text))
+          .then(res => res.json())
+          .then(json => {console.log(json); setResults(json.photos)})
         e.preventDefault();
       }}>
         <input onChange={e => setQuery(e.target.value)} />
         <input type="submit" value="Search" />
       </form>
+      <ol style={{listStyle: "none", display: "flex", flexWrap: "wrap", padding: 0}}>
+      {results.map((res, idx) => <li key={idx} style={{flexBasis: 100, flexGrow: 1, height: 100}}><img src={res.src.original} style={{maxWidth: "100%", maxHeight: "100%"}} /></li>)}
+      </ol>
     </>
   );
 }
