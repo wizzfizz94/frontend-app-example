d9747178d3e7fc35ede1bfd699e10ce6eab74eb1
wizzfizz94 <21149116@student.uwa.edu.au>
2022-05-05 09:28:58 +0800

    Reorganised routing.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import { Outlet } from "react-router-dom";
 import './App.css';
 import Search from "./Search";
 import NameForm from "./NameForm";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   const [name, setName] = useState(null);
 
   return (
-    <BrowserRouter>
-      <main className="container">
-        <Routes>
-          <Route path="/" element={<NameForm setNameHandler={name => setName(name)} />} />
-          <Route path="search" element={<Search name={name} />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <main className="container">
+      <Routes>
+        <Route path="/" element={<NameForm setNameHandler={name => setName(name)} />} />
+        <Route path="search" element={<Search name={name} />} />
+      </Routes>
+    </main>
   );
 }
 
